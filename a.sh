c9144651874b49f29d16e3fb61352e99d971b248
Weslley Barros <weslley17w@gmail.com>
2021-07-10 10:57:28 -0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,4 @@
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
