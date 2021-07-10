93cfcf6d918ae4ccca8b7d1d5e05c869e24e5b14
Weslley Barros <weslley17w@gmail.com>
2021-07-10 11:10:46 -0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -2,3 +2,5 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i -e 's/robbyrussell/af-magic/g' ~/.zshrc
+source .zshrc #RECARREGAR TERMINAL FINAL
